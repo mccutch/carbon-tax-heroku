5f7c066e5e0ae02d303d8188b91cdf038f94e4ab
Jack McCutchan <jack.mccutchan@gmail.com>
2020-05-15 10:42:47 -0700

    tested heroku local

--- backend/settings.py
+++ backend/settings.py
@@ -97,8 +97,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PW'),
