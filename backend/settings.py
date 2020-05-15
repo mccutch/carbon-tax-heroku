0e7e19dc5763db687b785d6e621ed438b3c4034c
Jack McCutchan <jack.mccutchan@gmail.com>
2020-05-15 10:36:08 -0700

    migrate db to postgres

--- backend/settings.py
+++ backend/settings.py
@@ -96,8 +96,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PW'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
