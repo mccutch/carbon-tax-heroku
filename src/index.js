282fe438aaacd281c7d7c9e1f1ca8c9ccab71ded
Jack McCutchan <jack.mccutchan@gmail.com>
2020-09-08 13:10:26 -0700

    move sw to public

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import {MainView} from './mainView.js';
 import {NavBar} from './navBar.js';
 import {LoginForm, logoutBrowser, demoLogin} from './loginWrapper.js';
 import {RegistrationForm} from './registrationForm.js';
-import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from './serviceWorker.js';
 
 import {GoogleDirections} from './googleDirections.js';
 
@@ -294,4 +294,4 @@ ReactDOM.render(<App />, document.getElementById('root'))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+//serviceWorker.register();
