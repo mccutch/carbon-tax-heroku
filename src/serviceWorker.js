535fe6c62b9852e0d10a0565a0c919607bce7b8d
Jack McCutchan <jack.mccutchan@gmail.com>
2020-09-08 19:35:17 -0700

    try to use default sw

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -32,7 +32,7 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/sw.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
       //const swUrl = `./sw.js`;
 
       if (isLocalhost) {
